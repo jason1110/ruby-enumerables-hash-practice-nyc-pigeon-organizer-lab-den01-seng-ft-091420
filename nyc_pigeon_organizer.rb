20c9f39c97811c127f4aa9c91c4556242f316cc4
Jason Easterly <jason.easterly11@gmail.com>
2020-08-26 19:46:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -31,10 +31,7 @@ data.each_with_object({}) do |(key, value), new_hash|
       if !new_hash[name_value][key]
         new_hash[name_value][key] = []
       end
-binding.pry
-      if !new_hash[name_value][key][value]
-        new_hash[name_value][key][value] = value.push(value.to_s)
-      end  
+        new_hash[name_value][key][value].push(k1.to_s)
 binding.pry   
     end
   end
